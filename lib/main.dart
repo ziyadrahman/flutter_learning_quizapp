d5ce7e7f995c496dfed78473a4de48fecc4537d9
ziyadrahman <ziyad.rahman@icloud.com>
2024-07-11 12:18:29 +0530

    UI Added Successfully

--- lib/main.dart
+++ lib/main.dart
@@ -86,6 +86,19 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         //TODO: Add a Row here as your score keeper
+
+        const Row(
+          children: <Widget>[
+            Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+            Icon(
+              Icons.close,
+              color: Colors.red,
+            )
+          ],
+        )
       ],
     );
   }

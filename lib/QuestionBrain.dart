264db7b9e4362d7eb9612d5fabf5c0f4e7302a65
ziyadrahman <ziyad.rahman@icloud.com>
2024-07-25 12:53:55 +0530

    Encapsulation Implemented

--- lib/QuestionBrain.dart
+++ lib/QuestionBrain.dart
@@ -3,6 +3,7 @@ import 'Question.dart';
 class Questionbrain {
   int questionNumber = 0;
 
+  //Private Variable defined by (_)underscore which is not accessible for other classes
   final List<Question> _questionAnsList = [
     Question('You can lead a cow down stairs but not up stairs.', true),
     Question(
@@ -10,6 +11,7 @@ class Questionbrain {
     Question('A slug\'s  blood is green.', true)
   ];
 
+  //Public Methods with controlled access
   String get questionText => _questionAnsList[questionNumber].text;
   // int get questionNumber => _questionNumber;
   bool get questionAnswer => _questionAnsList[questionNumber].answer;
